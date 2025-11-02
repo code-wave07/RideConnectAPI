8c258b65568c89d8c087d644d01cc404c5f8d5d0
Onyekachi <onwudinjopatrick5@gmail.com>
2025-11-02 18:49:37 +0100

    edited the registeration requests and removed comfirmpassword

--- RideConnect.Models/Requests/CustomerRegistrationRequest.cs
+++ RideConnect.Models/Requests/CustomerRegistrationRequest.cs
@@ -8,6 +8,6 @@ public class CustomerRegistrationRequest
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
+    //public string ConfirmPassword { get; set; }
     public bool Active { get; set; }
 }
