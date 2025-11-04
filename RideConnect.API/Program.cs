2148cc3c76a3cb54bd7c3ac579b6d6488008298f
Onyekachi <onwudinjopatrick5@gmail.com>
2025-11-04 14:46:09 +0100

    added cors configuration

--- RideConnect.API/Program.cs
+++ RideConnect.API/Program.cs
@@ -73,6 +73,22 @@ builder.Services.AddAuthorization(cfg =>
     cfg.AddPolicy("Authorization", policy => policy.Requirements.Add(new AuthorizationRequirment()));
 });
 
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder => {
+    builder.AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins(
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
+        "http://localhost:3003",
+        "http://localhost:3004",
+        "http://localhost:5500",
+        "http://localhost:3005" )
+        
+        .AllowCredentials();
+
+}));
+
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers();
@@ -94,6 +110,8 @@ if (app.Environment.IsDevelopment())
         c.InjectStylesheet("/css/swagger-dark-theme.css");
     });
 }
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
